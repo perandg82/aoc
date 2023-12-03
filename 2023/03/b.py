7552d5123dc10dbf04c0c0edb163aef0d79d26b1
Per Andreas Gulbrandsen <per.andreas.gulbrandsen@nomono.co>
2023-12-03 18:28:32 +0100

    identify correct asterix positions

--- 2023/03/b.py
+++ 2023/03/b.py
@@ -2,45 +2,34 @@
 
 import re
 
-def find_indexes(line)
+def find_indexes(line):
     i = 0
     nums = []
     while i < len(line):
         try:
-            nums.append(line[i:].index("*"))
+            match = line[i:].index("*")
+            nums.append(match)
+            i+=match+1
         except ValueError:
             break
     return nums
 
-def find_numbers(line):
+def find_nr_numbers(pattern):
     i = 0
-    nums = []
-    while i < len(line):
-        if line[i].isdigit():
-            s = i 
-            try:
-                while line[i].isdigit():
-                    i+=1
-            except IndexError:
-                pass
-            nums.append((s,i))
-        i += 1
-    return nums
-
-
-def has_symbol(pattern):
-    print("Testing {}".format(pattern))
-    for c in pattern:
-        if c != "." and not c.isdigit():
-            print("Abort on char {}".format(c))
-            return True
-    return False
+    nums = 0
+    t = [pattern[0].isdigit(), pattern[1].isdigit(), pattern[2].isdigit()]
+    if t.count(True) == 0:
+        return 0
+    elif t == [True, False, True]:
+        print("{} returning 2".format(t))
+        return 2
+    print("{} returning 1".format(t))
+    return 1
 
 #f = open("testinput.txt", "r")
 f = open("inputa.txt", "r")
 d = f.readlines()
 f.close()
-print(d)
 sum = 0
 t = "."*len(d[0])
 d.insert(0,t)
@@ -50,5 +39,16 @@ for line in d[1:-1]:
     index = d.index(line)
     last_index = len(line)-1
     nums = find_indexes(line)
+    for num in nums:
+        nr_nrs = 0
+        nr_nrs += find_nr_numbers(d[index-1][num-1:num+2])
+        nr_nrs += find_nr_numbers(d[index][num-1:num+2])
+        nr_nrs += find_nr_numbers(d[index+1][num-1:num+2])
+        if nr_nrs == 2:
+            print("Found a possible match:")
+            print(d[index-1][num-4:num+5])
+            print(d[index][num-4:num+5])
+            print(d[index+1][num-4:num+5])
+
 
 print(sum)
