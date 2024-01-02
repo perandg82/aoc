ac6943a6a8015b1575165061645a81b3b8b9ed23
Per Andreas Gulbrandsen <peran.gulbrandsen@zolve.no>
2024-01-02 10:16:33 +0100

    day 6

---
+++ 2023/06/a.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import re
+
+pattern = re.compile("^\D*(\d)")
+
+f = open("inputa.txt", "r")
+d = f.readlines()
+f.close()
+
+times = []
+for elem in d[0][:-1].split(" "):
+    times.append(int(elem))
+
+dists = []
+for elem in d[1][:-1].split(" "):
+    dists.append(int(elem))
+
+#times = [7,15,30]
+#dists = [9,40,200]
+
+print(times)
+print(dists)
+
+sums = []
+
+for i in range(len(dists)):
+    time = times[i]
+    sum = 0
+    for j in range(time-1, 0, -1):
+        speed = j
+        distance_traveled = speed * (time-j)
+        if distance_traveled > dists[i]:
+            print("  VALID: {}, {} > {}".format(j, distance_traveled, dists[i]))
+            sum += 1
+        else:
+            print("INVALID: {}, {} < {}".format(j, distance_traveled, dists[i]))
+    sums.append(sum)
+
+print(sums)
+sum = 1
+for s in sums:
+    sum = sum*s
+print(sum)
