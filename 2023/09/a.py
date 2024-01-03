72d8c1a6a168710ba0597e168d81b80d70ecd8d3
Per Andreas Gulbrandsen <peran.gulbrandsen@zolve.no>
2024-01-03 16:10:18 +0100

    close to working part a day 9

---
+++ 2023/09/a.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import re
+def find_val(d):
+    print("Finding val for {}".format(d))
+    index = 0
+    while True:
+        new_line = []
+        line = d[index]
+        #print("About to start iter {}, d len is {}, line len is {}".format(index, len(d), len(line)))
+        index+=1
+        for i in range(len(line)-1):
+            new_line.append(line[i+1]-line[i])
+        if sum(new_line) == 0:
+            d.append(new_line)
+            #print("Appended new line")
+            print("Done!")
+            #print(d)
+            break
+        else:
+            d.append(new_line)
+            #print("Appended new line")
+            #print(new_line)
+
+    for i in range(len(d)-1, -1, -1):
+        if i == len(d)-1:
+            d[i].append(0)
+        else:
+            d[i].append(d[i][-1] + d[i+1][-1])
+    for line in d:
+        print(line)
+    return d[0][-1]
+
+pattern = re.compile("^\D*(\d)")
+
+f = open("inputa.txt", "r")
+#f = open("testinput.txt", "r")
+d = f.readlines()
+f.close()
+
+#print(find_val([[0,3,6,9,12,15]]))
+#print(find_val([[1,3,6,10,15,21]]))
+#print(find_val([[10,13,16,21,30,45]]))
+
+score = []
+for line in d:
+    d_int = []
+    for w in line.split(" "):
+        d_int.append(int(w))
+    score.append(find_val([d_int]))
+print("Len is {} and sum is {}".format(len(score), sum(score)))
